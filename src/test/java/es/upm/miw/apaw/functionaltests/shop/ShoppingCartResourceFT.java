d30fe86b5f94d404006479c5dbd253ea4b8ab850
JBernal <j.bernal@upm.es>
2025-09-21 19:31:15 +0200

    formated code

--- src/test/java/es/upm/miw/apaw/functionaltests/shop/ShoppingCartResourceFT.java
+++ src/test/java/es/upm/miw/apaw/functionaltests/shop/ShoppingCartResourceFT.java
@@ -3,8 +3,6 @@ package es.upm.miw.apaw.functionaltests.shop;
 import es.upm.miw.apaw.functionaltests.UserDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
